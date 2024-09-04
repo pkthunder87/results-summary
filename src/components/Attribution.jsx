e6a8377f4b592bfc02c1767404802f61041385ee
pkthunder87 <pkthunder87@gmail.com>
2024-09-04 13:29:12 -0700

    Feat: Add mobile static of Results.jsx

--- src/components/Attribution.jsx
+++ src/components/Attribution.jsx
@@ -1,10 +1,17 @@
 function Attribution() {
   return (
     <div className="attribution">
-      <a href="https://www.frontendmentor.io/challenges/results-summary-component-CE_K6s0maV">
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://www.frontendmentor.io/challenges/results-summary-component-CE_K6s0maV"
+      >
         Frontend Mentor
       </a>
-      . Coded by <a href="#">Your Name Here</a>.
+      . Coded by{" "}
+      <a href="#" target="_blank" rel="noreferrer">
+        Your Name Here
+      </a>
     </div>
   );
 }
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,5 +1,17 @@
 function Results() {
-  return <section className="results">Your Results</section>;
+  return (
+    <section className="results">
+      <h2 className="results__title">Your Results</h2>
+      <div className="results__circle">
+        <h3 className="results__score">76</h3>
+        <p className="results__of">of 100</p>
+      </div>
+      <h3 className="results__eval">Great</h3>
+      <p className="results__text">
+        You scored higher than 65% of the people who have taken these tests.
+      </p>
+    </section>
+  );
 }
 
 export default Results;
